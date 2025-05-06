d180839a79d35238e2234bd4e295f14d532b6c6e
DannyNguyen0411 <90318109+DannyNguyen0411@users.noreply.github.com>
2025-05-06 11:30:03 +0200

    Added more things into it

--- src/main.java
+++ src/main.java
@@ -1,2 +1,13 @@
+import java.util.Scanner;
+
 public class main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+        // Import for the user of different classes
+
+        System.out.println("Welkom bij de Scrum End Game. Ben je klaar voor de EndGame?\nWat is je naam?");
+        String name = scanner.nextLine();
+        System.out.println("Speler " + name + " is er klaar voor!");
+
+    }
 }
